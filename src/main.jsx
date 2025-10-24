e3c9483abafe86a28be1a15c0e673893d191d65c
Camila Becerra <cami.becerrac@duocuc.cl>
2025-10-24 02:03:29 -0300

    Usar HashRouter para compatibilidad con GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext"; // si lo usas
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./styles/style.css"; // ruta correcta: src/styles/style.css
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
