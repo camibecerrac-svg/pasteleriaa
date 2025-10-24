2b4d69c5c558aee0382087ba1fceb727a85bac67
Camila Becerra <cami.becerrac@duocuc.cl>
2025-10-24 03:07:47 -0300

    Actualizar configuración de Vite y restaurar index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pasteleriaa/', // 👈 EXACTAMENTE igual al nombre de tu repo
+  base: '/pasteleriaa/', // 👈 debe ser el nombre EXACTO del repo en GitHub
 })
